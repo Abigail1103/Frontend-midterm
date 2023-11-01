19df1193f73f7f569f4412e32ba9dafabccf17d0
Alva <xshinoa0208@gmail.com>
2023-11-01 18:33:05 +0800

    add menupopup

--- js/script.js
+++ js/script.js
@@ -1,26 +1,61 @@
 // alert("QQ");
 let about =document.querySelector("#about");
+let menu =document.querySelector("#menu");
+let reservation =document.querySelector("#reservation");
 let popupabout = document.querySelector("#popupabout");
+let popupmenu = document.querySelector("#popupmenu");
 let isAboutHover =false;
 let isAboutOpen =false;
+let isMenuHover =false;
+let isMenuOpen =false;
+let isReservationHover =false;
 
 $(document).ready(function() {
     if(!isAboutHover){
         $('#about').mouseenter(function() {
+            $('#popupmenu').slideUp(300);
             $('#popupabout').slideDown(300);
             isAboutHover=true;
             // $('#popupabout').css('display', 'block');
         }).mouseleave(function(){
             isAboutHover=false;
+            if(!isAboutOpen){
+                $('#popupabout').mouseenter(function(){
+                    $('#popupabout').css('display','block');
+                    isAboutOpen=true;
+                }).mouseleave(function(){
+                    $('#popupabout').slideUp(300);
+                    isAboutOpen=false;
+                })
+            }
+            
         })
-        $('#popupabout').mouseenter(function(){
-            $('#popupabout').css('display','block');
-            isAboutOpen=true;
-        }).mouseleave(function(){
+    }
+    if(!isMenuHover){
+        $('#menu').mouseenter(function() {
             $('#popupabout').slideUp(300);
-            isAboutOpen=false;
+            $('#popupmenu').slideDown(300);
+            isMenuHover=true;
+            // $('#popupabout').css('display', 'block');
+        }).mouseleave(function(){
+            isMenuHover=false;
+            if(!isMenuOpen){
+                $('#popupmenu').mouseenter(function(){
+                    $('#popupmenu').css('display','block');
+                    isMenuOpen=true;
+                }).mouseleave(function(){
+                    $('#popupmenu').slideUp(300);
+                    isMenuOpen=false;
+                })
+            }
+            
         })
-
-    };
+    }
+    if(!isReservationHover){
+        $("#reservation").hover(function(){
+            $('#popupmenu').slideUp(300);
+            $('#popupabout').slideUp(300);
+        })
+    }
     
 });
