63a7936f74feefcadba7f2fd4577caf37526fc3f
Alva <xshinoa0208@gmail.com>
2023-12-29 02:33:38 +0800

    qaOK

--- js/script.js
+++ js/script.js
@@ -152,10 +152,18 @@ $('document').ready(function(){
     $("#qa2").click(function(){
         if(!isAnswerOpen){
             $("#ans2").slideDown(300);
+            $("#qa2").css('border-color','#A4907C').css('background-color','#8B706A').css('border-radius','5px');
+            $("#cont2").css('color','#fff');
+            $("#ques_num2").css('background-color','#fff').css('color','#6F553E');
+            $("#ques_title2").css('color','#fff');
             $("#btn_qa2").css('transform','rotate(-45deg)').css('transition','all 0.5s');
             isAnswerOpen=true;
         }else{
             $("#ans2").slideUp(300);
+            $("#qa2").css('border-color','#8B706A').css('background-color','#fff').css('border-radius','0px').css('transition','all 0.5s');
+            $("#cont2").css('color','#A4907C');
+            $("#ques_num2").css('background-color','#6F553E').css('color','#fff');
+            $("#ques_title2").css('color','#6F553E');
             $("#btn_qa2").css('transform','rotate(0deg)').css('transition','all 0.5s');
             isAnswerOpen=false;
         }
@@ -165,10 +173,18 @@ $('document').ready(function(){
     $("#qa3").click(function(){
         if(!isAnswerOpen){
             $("#ans3").slideDown(300);
+            $("#qa3").css('border-color','#A4907C').css('background-color','#8B706A').css('border-radius','5px');
+            $("#cont3").css('color','#fff');
+            $("#ques_num3").css('background-color','#fff').css('color','#6F553E');
+            $("#ques_title3").css('color','#fff');
             $("#btn_qa3").css('transform','rotate(-45deg)').css('transition','all 0.5s');
             isAnswerOpen=true;
         }else{
             $("#ans3").slideUp(300);
+            $("#qa3").css('border-color','#8B706A').css('background-color','#fff').css('border-radius','0px').css('transition','all 0.5s');
+            $("#cont3").css('color','#A4907C');
+            $("#ques_num3").css('background-color','#6F553E').css('color','#fff');
+            $("#ques_title3").css('color','#6F553E');
             $("#btn_qa3").css('transform','rotate(0deg)').css('transition','all 0.5s');
             isAnswerOpen=false;
         }
@@ -178,10 +194,18 @@ $('document').ready(function(){
     $("#qa4").click(function(){
         if(!isAnswerOpen){
             $("#ans4").slideDown(300);
+            $("#qa4").css('border-color','#A4907C').css('background-color','#8B706A').css('border-radius','5px');
+            $("#cont4").css('color','#fff');
+            $("#ques_num4").css('background-color','#fff').css('color','#6F553E');
+            $("#ques_title4").css('color','#fff');
             $("#btn_qa4").css('transform','rotate(-45deg)').css('transition','all 0.5s');
             isAnswerOpen=true;
         }else{
             $("#ans4").slideUp(300);
+            $("#qa4").css('border-color','#8B706A').css('background-color','#fff').css('border-radius','0px').css('transition','all 0.5s');
+            $("#cont4").css('color','#A4907C');
+            $("#ques_num4").css('background-color','#6F553E').css('color','#fff');
+            $("#ques_title4").css('color','#6F553E');
             $("#btn_qa4").css('transform','rotate(0deg)').css('transition','all 0.5s');
             isAnswerOpen=false;
         }
@@ -191,10 +215,18 @@ $('document').ready(function(){
     $("#qa5").click(function(){
         if(!isAnswerOpen){
             $("#ans5").slideDown(300);
+            $("#qa5").css('border-color','#A4907C').css('background-color','#8B706A').css('border-radius','5px');
+            $("#cont5").css('color','#fff');
+            $("#ques_num5").css('background-color','#fff').css('color','#6F553E');
+            $("#ques_title5").css('color','#fff');
             $("#btn_qa5").css('transform','rotate(-45deg)').css('transition','all 0.5s');
             isAnswerOpen=true;
         }else{
             $("#ans5").slideUp(300);
+            $("#qa5").css('border-color','#8B706A').css('background-color','#fff').css('border-radius','0px').css('transition','all 0.5s');
+            $("#cont5").css('color','#A4907C');
+            $("#ques_num5").css('background-color','#6F553E').css('color','#fff');
+            $("#ques_title5").css('color','#6F553E');
             $("#btn_qa5").css('transform','rotate(0deg)').css('transition','all 0.5s');
             isAnswerOpen=false;
         }
@@ -204,10 +236,18 @@ $('document').ready(function(){
     $("#qa6").click(function(){
         if(!isAnswerOpen){
             $("#ans6").slideDown(300);
+            $("#qa6").css('border-color','#A4907C').css('background-color','#8B706A').css('border-radius','5px');
+            $("#cont6").css('color','#fff');
+            $("#ques_num6").css('background-color','#fff').css('color','#6F553E');
+            $("#ques_title6").css('color','#fff');
             $("#btn_qa6").css('transform','rotate(-45deg)').css('transition','all 0.5s');
             isAnswerOpen=true;
         }else{
             $("#ans6").slideUp(300);
+            $("#qa6").css('border-color','#8B706A').css('background-color','#fff').css('border-radius','0px').css('transition','all 0.5s');
+            $("#cont6").css('color','#A4907C');
+            $("#ques_num6").css('background-color','#6F553E').css('color','#fff');
+            $("#ques_title6").css('color','#6F553E');
             $("#btn_qa6").css('transform','rotate(0deg)').css('transition','all 0.5s');
             isAnswerOpen=false;
         }
@@ -217,10 +257,18 @@ $('document').ready(function(){
     $("#qa7").click(function(){
         if(!isAnswerOpen){
             $("#ans7").slideDown(300);
+            $("#qa7").css('border-color','#A4907C').css('background-color','#8B706A').css('border-radius','5px');
+            $("#cont7").css('color','#fff');
+            $("#ques_num7").css('background-color','#fff').css('color','#6F553E');
+            $("#ques_title7").css('color','#fff');
             $("#btn_qa7").css('transform','rotate(-45deg)').css('transition','all 0.5s');
             isAnswerOpen=true;
         }else{
             $("#ans7").slideUp(300);
+            $("#qa7").css('border-color','#8B706A').css('background-color','#fff').css('border-radius','0px').css('transition','all 0.5s');
+            $("#cont7").css('color','#A4907C');
+            $("#ques_num7").css('background-color','#6F553E').css('color','#fff');
+            $("#ques_title7").css('color','#6F553E');
             $("#btn_qa7").css('transform','rotate(0deg)').css('transition','all 0.5s');
             isAnswerOpen=false;
         }
